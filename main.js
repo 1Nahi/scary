0fc211cca25cabe3bd05011c9c474fb72a6bb6c1
1Nahi <adwityadeep@gmail.com>
2021-12-21 15:55:02 +0530

    *

---
+++ main.js
@@ -0,0 +1,85 @@
+const navsliding = document.querySelector(".sliding-navbar");
+const navBtn = document.querySelector(".navicon");
+const navSelect = document.querySelectorAll(".nav-text");
+isOpen = false;
+navBtn.addEventListener("click", function() {
+  if (isOpen == false) {
+    isOpen = true;
+    navsliding.classList.add("open");
+  }
+  else {
+    isOpen = false;
+    navsliding.classList.remove("open");
+  }
+});
+
+const showMsg = () => {
+  Swal.fire({
+    title: '<img src="https://img.icons8.com/dusk/64/000000/ghost--v1.png"/>',
+    width: 600,
+    padding: '3em',
+    color: '#4F4165',
+    background: '#716add',
+    confirmButtonText: '👀',
+    confirmButtonColor: '#4F4165',
+    backdrop: `
+        rgba(0,0,123,0.4)
+        url("https://i.gifer.com/5N2g.gif")
+        top
+        no-repeat
+      `
+  })
+}
+const showMsg2 = () => {
+  Swal.fire({
+    title: '<img src="https://img.icons8.com/dusk/64/000000/ghost--v1.png"/>',
+    width: 600,
+    padding: '3em',
+    color: '#4F4165',
+    background: '#716add',
+    confirmButtonText: '👀',
+    confirmButtonColor: '#4F4165',
+    backdrop: `
+        rgba(0,0,123,0.4)
+        url("https://i.gifer.com/3Ua7.gif")
+        top
+        no-repeat
+      `
+  })
+}
+const showMsg3 = () => {
+  Swal.fire({
+    title: '<img src="https://img.icons8.com/dusk/64/000000/ghost--v1.png"/>',
+    width: 600,
+    padding: '3em',
+    color: '#4F4165',
+    background: '#716add',
+    confirmButtonText: '👀',
+    confirmButtonColor: '#4F4165',
+    backdrop: `
+        rgba(0,0,123,0.4)
+        url("https://i.pinimg.com/originals/a6/ce/bf/a6cebfa2db13098c4bf7a636ee696a48.gif")
+        top
+        no-repeat
+      `
+  })
+}
+const showMsg4 = () => {
+  Swal.fire({
+    title: '<img src="https://img.icons8.com/dusk/64/000000/ghost--v1.png"/>',
+    width: 600,
+    padding: '3em',
+    color: '#4F4165',
+    background: '#716add',
+    confirmButtonText: '👀',
+    confirmButtonColor: '#4F4165',
+    backdrop: `
+        rgba(0,0,123,0.4)
+        url("https://c.tenor.com/LnUFxubwuegAAAAC/ghostface-killah.gif")
+        top
+        no-repeat
+      `
+  })
+}
+
+
